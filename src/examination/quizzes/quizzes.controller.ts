12cc8c31230b9ff4df8355806d5bb329b8a58ce8
hawng04 <nguyenthithanhhangqn2004@gmail.com>
2026-02-11 22:16:02 +0700

    quizzes

--- src/examination/quizzes/quizzes.controller.ts
+++ src/examination/quizzes/quizzes.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { CreateQuizDto } from './dto/create-quiz.dto';
 import { QuizzesService } from './quizzes.service';
 import { UpdateQuizDto } from './dto/update-quiz.dto';
+import { JwtAuthGuard } from 'src/iam/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/iam/auth/guards/roles.guard';
+import { Roles } from 'src/iam/auth/decorators/roles.decorator';
 
 @ApiTags('Quizzes - Quản lý Đề thi') // Gom nhóm trong Swagger
+@ApiBearerAuth() // Hiện nút Authorize trong Swagger
 @Controller('quizzes')
 export class QuizzesController {
   constructor(private readonly quizzesService: QuizzesService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles ('ADMIN', 'INSTRUCTOR') 
   @ApiOperation({ summary: 'Tạo đề thi mới (Kèm câu hỏi & đáp án)' })
   create(@Body() createQuizDto: CreateQuizDto) {
     return this.quizzesService.create(createQuizDto);
@@ -28,12 +34,16 @@ export class QuizzesController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles ('ADMIN', 'INSTRUCTOR')
   @ApiOperation({ summary: 'Cập nhật đề thi (Sửa tên, thời gian, mô tả...)' })
   update(@Param('id') id: string, @Body() updateQuizDto: UpdateQuizDto) {
     return this.quizzesService.update(+id, updateQuizDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles ('ADMIN', 'INSTRUCTOR')
   @ApiOperation({ summary: 'Xóa đề thi' })
   remove(@Param('id') id: string) {
     return this.quizzesService.remove(+id);
